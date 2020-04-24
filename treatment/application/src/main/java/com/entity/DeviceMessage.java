b1be0da6a0cca550c41b31ca46a15c1f3ea08ea1
kohen <kohen.xi@maxnerva.com>
2020-04-24 18:02:58 +0800

    协议定义

---
+++ treatment/application/src/main/java/com/entity/DeviceMessage.java
@@ -0,0 +1,25 @@
+package com.entity;
+
+import lombok.Data;
+
+import java.util.Date;
+import java.util.HashMap;
+
+/**
+ * @ClassName DeviceMessage
+ * @Description: TODO
+ * @Author xizhonghuai
+ * @Date 2020/4/24
+ * @Version V1.0
+ **/
+@Data
+public class DeviceMessage {
+
+    //    private String userId;
+//    private String orderId;
+//    private String serialId;
+
+    private String id;
+    private HashMap<String, Object> body;
+    private Date date = new Date();
+}
